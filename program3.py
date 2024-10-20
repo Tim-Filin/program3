31028fee8cae2235b5346b4f87a557fe09ec7f94
Tim-Filin <geyfulintimur@gmail.com>
2024-10-21 01:07:33 +0300

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,9 @@
+dz = 12
+print(dz)
+hs = 1.5
+print(hs)
+curs = 'Python'
+print(curs)
+time = hs / dz
+print(time)
+print('Курc:',curs,'Всего задач:', dz, 'затрачено часов:', hs, 'среднее время выполнения:', time)
